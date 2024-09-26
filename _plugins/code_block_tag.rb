da41145b3b460c621e8bff72623907468b361529
ib-bsb-br <marioseixascardosoneto@gmail.com>
2024-09-26 14:05:57 -0300

    Update code_block_tag.rb

--- _plugins/code_block_tag.rb
+++ _plugins/code_block_tag.rb
@@ -41,7 +41,7 @@ module Jekyll
       escaped_code_content = code_content.gsub('"', '&quot;')
       
       <<~HTML
-        "<section data-filename=\"#{filename}\" data-code=\"#{escaped_code_content}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language} line-numbers\" style=\"white-space:pre-wrap;\"><code class=\"language-#{@language}\">#{code_content}</code></section>"
+        <section data-filename=\"#{filename}\" data-code=\"#{escaped_code_content}\" data-download-link data-download-link-label=\"#{download_label}\" class=\"language-#{@language} line-numbers\" style=\"white-space:pre-wrap;\"><code class=\"language-#{@language}\">#{code_content}</code></section>
       HTML
     end
   end
